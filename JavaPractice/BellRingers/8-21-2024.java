eb950fbb29629bca245a21031ce6ca90a2191d3b
LeopardDrager <maguirewmosby@gmail.com>
2024-08-21 13:37:52 -0500

    Added sections into the JavaPractice. Also added todays bellringer to the bellringer folder.

---
+++ JavaPractice/BellRingers/8-21-2024.java
@@ -0,0 +1,12 @@
+public class Bellringer {
+    public static void main(String[] args) {
+        System.out.println("decialmal\thexeadecimal\tcharacter");
+        System.out.println("---------\t------------\t---------");
+        System.out.println("97\t\t61\t\ta");
+        System.out.println("98\t\t62\t\tb");
+        System.out.println("99\t\t63\t\tc");
+        System.out.println("100\t\t64\t\td");
+        System.out.println("101\t\t65\t\te");
+    
+    }    
+}
