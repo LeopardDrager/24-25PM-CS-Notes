c93d724191a883ec3bb36f6c26fb2ce5acaca94e
LeopardDrager <maguirewmosby@gmail.com>
2024-08-21 13:40:17 -0500

    Added java projects into notes

---
+++ JavaPractice/Notes/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+    //coment out one line of code
+    /* 
+    this is a block comment
+    */   
+    public static void main(String[] args){
+ 
+        System.out.println("cool");
+        }
+}
