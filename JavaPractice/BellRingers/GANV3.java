d43556d5a0edc699f3ac8ae8118b93cbbcf9e560
LeopardDrager <maguirewmosby@gmail.com>
2024-09-11 09:44:06 -0500

    Added GANV3

---
+++ JavaPractice/BellRingers/GANV3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GANV3 {
+    public static void main(String[] args){
+
+        Scanner ui = new Scanner(System.in);    
+        int i = 1;
+        
+        while(i != 0){ // I used a while loop because it was easier to make that repeat forever than a for statement
+        //it would be nice to clean our user input... Because we have suspicious users...
+        Random generator = new Random();
+        int numberGenerated = generator.nextInt(0,11);
+       
+        System.out.print("Please pick a number from 1-10 ");
+        int guess = ui.nextInt();
+
+        if (guess > numberGenerated ){
+            System.out.println("The number was smaller than your guess");
+        }else if (guess < numberGenerated){
+            System.out.println("The number was bigger than your guess");
+        }else{
+            System.out.println("You got it, good job!");
+            i = 0;
+        }
+    }
+    ui.close();}
+}
