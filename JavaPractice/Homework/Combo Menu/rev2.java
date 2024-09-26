0847259a74a62a8e5769c6e50316e56301520334
LeopardDrager <maguirewmosby@gmail.com>
2024-09-26 09:04:32 -0500

    Added combo menue rev2, need to add prices calculation

---
+++ JavaPractice/Homework/Combo Menu/rev2.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class rev2   {
+    public static void main(String[] args) {
+        
+    
+        String tempSand = orderSandwhich();
+        String tempFries = orderFries();
+        String tempDrink = orderDrink();
+        System.out.println( "You got a " +tempSand+" "+tempFries + " "+tempDrink);
+
+    }
+
+
+         
+    private static String orderSandwhich(){ // asking user if they want a sandwhich in case they don't
+        String tempSand ="";
+        Scanner ui = new Scanner(System.in);    
+        System.out.println("Would you like a sandwhich? y/n " );
+        String sandwhichChoice = ui.next();
+
+        if (sandwhichChoice.equalsIgnoreCase("y")){
+            
+            System.out.println("The prices are \n 1. Tofu: $5.75 \n 2. Chicken: $5.25 \n 3. Beef: $6.25 \n 4. None of these");
+            int toppingChoice = ui.nextInt();
+
+            if(toppingChoice == 1){            
+                tempSand="tofu sandwhich";
+            }else if(toppingChoice == 2){
+                tempSand="chicken sandwich";
+            }else if(toppingChoice == 3) {
+                tempSand="beef sandwhich";
+            }
+        }else{
+            System.out.println("I guess you don't want a sandwhich.");
+        }
+        return tempSand;
+    }
+    
+
+    private static String orderFries(){ // asking user if they want fry
+        String tempFries = "";
+        Scanner ui = new Scanner(System.in);
+        System.out.println("Would a like some fries? y/n ");
+        String fryChoice = ui.next();
+        
+        if (fryChoice.equalsIgnoreCase("y")){
+            System.out.println("The prices are \n 1. Small: $1.00 \n 2. Medium: $1.75 \n 3. Large: $2.25 \n 4. None of these");
+            int sizeChoice = ui.nextInt();
+                if(sizeChoice == 1){            
+                    tempFries = "small fry";
+                }else if(sizeChoice == 2){
+                    tempFries = "medium fry";
+                }else if(sizeChoice == 3) {
+                    tempFries = "large fry";
+                }
+        }else {
+            System.out.println("I guess you don't want fries.");
+        }
+        return tempFries;
+        
+        }
+        private static String orderDrink(){ // asking user if they want fry
+            String tempDrink = "";
+            Scanner ui = new Scanner(System.in);
+            System.out.println("Would you like a drink? y/n ");
+            String drinkChoice = ui.next();
+            
+            if (drinkChoice.equalsIgnoreCase("y")){
+                System.out.println("The prices are \n 1. Small: $1.00 \n 2. Medium: $1.75 \n 3. Large: $2.25 \n 4. None of these");
+                int sizeChoice = ui.nextInt();
+                    if(sizeChoice == 1){            
+                        tempDrink = "small drink";
+                    }else if(sizeChoice == 2){
+                        tempDrink = "medium drink";
+                    }else if(sizeChoice == 3) {
+                        System.out.println("Would you like to upgrade to a child size for $0.38 more? y/n ");
+                        drinkChoice = ui.next();
+                        if(drinkChoice.equalsIgnoreCase("y")){
+                        tempDrink = "child size drink";
+                        }
+                        else{
+                            tempDrink = "large drink";
+                        }
+                    }
+            }else {
+                System.out.println("I guess you don't want a drink.");
+            }
+            return tempDrink;
+            
+            }
+}
