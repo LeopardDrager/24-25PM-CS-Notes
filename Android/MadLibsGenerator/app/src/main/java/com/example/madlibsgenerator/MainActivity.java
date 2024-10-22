9929b478897d1e9b2b0adcfd8f9c0c0b3ccd61eb
LeopardDrager <maguirewmosby@gmail.com>
2024-10-22 10:15:41 -0500

    Halfway done with Madlib generator

--- Android/MadLibsGenerator/app/src/main/java/com/example/madlibsgenerator/MainActivity.java
+++ Android/MadLibsGenerator/app/src/main/java/com/example/madlibsgenerator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.madlibsgenerator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +25,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button madLibChoiceOne = findViewById(R.id.optionOneMadLibBTN);
-        madLibChoiceOne.setOnClickListener(new View.OnClickListener() {
+        TextView outputTXT = findViewById(R.id.madLibPrintLocation);
+        EditText inputTXT = findViewById(R.id.response);
+        Button madLibChoiceOneBTN = findViewById(R.id.optionOneMadLibBTN);
+        Button madLibChoiceTwoBTN = findViewById(R.id.optionTwoMadLibBTN);
+        Button madLibChoiceThreeBTN = findViewById(R.id.optionThreeMadLibBTN);
+        Button generate = findViewById(R.id.generateBTN);
+
+
+        madLibChoiceOneBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                outputTXT.setText("Button one");
+                generate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String finishedMadLib = inputTXT.toString()
+
+                    }
+                });
 
             }
         });
 
+        madLibChoiceTwoBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputTXT.setText("Button two");
+
+            }
+        });
+        madLibChoiceThreeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputTXT.setText("Button three");
+            }
+        });
+
     }
+
 }
